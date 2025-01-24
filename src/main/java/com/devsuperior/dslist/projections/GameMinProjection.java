e14e111fc2bd330b39c7d17b55b7b810dd99dab1
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-24 11:29:33 -0300

    Homologo Deploy ok

--- src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -5,7 +5,7 @@ public interface GameMinProjection {
     Long getId(); // Tem que colcar métodos get para cada dado que a consulta
     // no banco está retornando
     String getTitle();
-    Integer getYear();
+    Integer getgameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -15,7 +15,7 @@ public interface GameRepository extends JpaRepository<Game, Long> { // Nome da e
     // Consulta SQL é sempre no repository, nativequery é o SQL padrão
     // o JPQUERY é outra linguagem de consulta da JPA
     @Query(nativeQuery = true, value = """ 
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 		tb_game.short_description AS shortDescription, tb_belonging.position
 		FROM tb_game
 		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
